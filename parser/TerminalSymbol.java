feb2304299b5dafccfbf1e6154a5139438bfa3df
Danny <danny@MacBook-Pro-15.local>
2019-02-05 15:27:52 -0500

    added the changes to the terminals

--- parser/TerminalSymbol.java
+++ parser/TerminalSymbol.java
@@ -1,7 +1,15 @@
 package parser;
 
+import java.util.List;
 // Author: Daniel Barbaro
 // An enumeration of the terminal symbol types
-public enum TerminalSymbol {
+public enum TerminalSymbol implements Symbol {
     VARIABLE, PLUS,  MINUS,  TIMES,  DIVIDE, OPEN, CLOSE;
+
+    public ParseState parse(List<Token> input) {
+        if(!input.isEmpty() && input.get(0).matches(this)) {
+            ParseState.build(true, LeafNode.build(input.get(0)), input.subList(1, input.size()));
+        }
+        return ParseState.FAILURE;
+    }
 }
